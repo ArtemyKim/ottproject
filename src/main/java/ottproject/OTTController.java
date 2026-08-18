683262aa7603e3638e1e4bae6e14b363db224e1a
yblee123 <decaleooo@gmail.com>
2026-08-18 12:11:43 +0900

    OTTController.java added

---
+++ src/main/java/ottproject/OTTController.java
@@ -0,0 +1,49 @@
+package ottproject;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet("*.do")
+public class OTTPageController extends HttpServlet {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String page = "otthome.jsp";
+		String uri = req.getRequestURI(); //http://localhost:8080/empapp/list.do
+		String requestUri = uri.substring(uri.lastIndexOf("/"), uri.length());
+		System.out.println("requestUri=" + requestUri); // request 요청 확인용(주소창에 뭘 입력했는지)
+		
+		switch (requestUri) {
+			case "/otthome.do" : {
+				page = "otthome.jsp";
+				break;
+			}
+			case "/ottpage.do" : {
+				// otthome.jsp에서의 animationId 변수를 가져온 후 가공하여 숫자만 남김. 이 값은 DB의 OTT_id와 같도록 할 것.
+				int ottid = Integer.parseInt(req.getParameter("ottId")); 
+			    System.out.println(ottid);
+			    
+			    List<OTTComment> ottCommentList = OTTCommentDAO.selectCommentsByOttId(ottid);
+			    OTTContent content = OTTContentDAO.selectContentById(ottid);
+			  
+			    req.setAttribute("curId", ottid); 
+			    req.setAttribute("ottContent", content); // 내장객체 request에 "ottContent" 라는 이름으로 ott객체를 저장함
+			    req.setAttribute("ottCommentList", ottCommentList); // 내장객체 request에 "ottContent" 라는 이름으로 ott객체를 저장함
+			    
+			    page = "ottpage.jsp";
+			    break;
+			}
+			
+			default :
+		}
+		RequestDispatcher rd = req.getRequestDispatcher(page);
+		rd.forward(req, resp);
+		
+	}
+}
